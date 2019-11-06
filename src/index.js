f3258203cd6a3160b060e1a5603b964e6b64d86b
王彦炳 <itwmike@hotmail.com>
2019-11-06 15:01:29 +0800

    LK 2.11.1 分支

--- src/index.js
+++ src/index.js
@@ -38,6 +38,7 @@ import Form from '../packages/form/index.js';
 import FormItem from '../packages/form-item/index.js';
 import Tabs from '../packages/tabs/index.js';
 import TabPane from '../packages/tab-pane/index.js';
+import LoTabPane from '../packages/lo-tab-pane/index.js';
 import Tag from '../packages/tag/index.js';
 import Tree from '../packages/tree/index.js';
 import Alert from '../packages/alert/index.js';
@@ -122,6 +123,7 @@ const components = [
   FormItem,
   Tabs,
   TabPane,
+  LoTabPane,
   Tag,
   Tree,
   Alert,
@@ -240,6 +242,7 @@ export default {
   FormItem,
   Tabs,
   TabPane,
+  LoTabPane,
   Tag,
   Tree,
   Alert,
