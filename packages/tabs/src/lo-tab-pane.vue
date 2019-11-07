91cc84a2cdb1e9b5c1f8c8cdad286ecefaddbabd
王彦炳 <itwmike@hotmail.com>
2019-11-07 10:34:13 +0800

    LK 2.11.1 分支

--- packages/tabs/src/lo-tab-pane.vue
+++ packages/tabs/src/lo-tab-pane.vue
@@ -2,7 +2,7 @@
     <div
             class="el-tab-pane"
             v-if="(!lazy || loaded) || active"
-            :style="{width: active ? '' : '0px', height: active ? '' : '0px', opacity: active ? '' : '0', visibility: active ? '' : 'hidden'}"
+            :style="{width: active ? '' : '0px', height: active ? '' : '0px', opacity: active ? '' : '0', position: active ? '' : 'absolute', left: active ? '' : '-100000px'}"
             role="tabpanel"
             :aria-hidden="!active"
             :id="`pane-${paneName}`"
